e2a637f5a2812015dfa6df51991e4b7917343233
majiajun <majiajun@unitedstack.com>
2015-05-15 14:39:37 +0800

    specialize for ued.ustack

---
+++ lib/templates/template.jsx
@@ -0,0 +1,9 @@
+define(['react', 'should be defined'], function(React, 'should be defined') {
+
+    'use strict';
+
+    var <%= _.classify(name) %> = {
+        'should do something';
+    }
+    return <%= _.classify(name) %>;
+});
